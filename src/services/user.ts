b2d31d6e2aab8d08726ccce63d712a80d5e5d1ef
Nico <50899946+nicsaul@users.noreply.github.com>
2021-12-06 13:56:45 -0300

    Feat/test endpoint (#19)
    * Add SearchOptions
    
    * Add category filter
    
    * Interface refactor
    
    * Goldfarb Test API

--- src/services/user.ts
+++ src/services/user.ts
@@ -4,14 +4,12 @@ import { IUser } from '../interfaces/user';
 const namespace = 'https://goldfarb:us:auth0:com';
 
 // Transforms user in session object to our IUser
-const transformUser = (user: Claims): IUser => {
-    return {
-        id: user.sub,
-        email: user.email,
-        name: user[`${namespace}/name`],
-        phone: user[`${namespace}/phone`],
-        cardcode: user[`${namespace}/cardcode`],
-    };
-};
+const transformUser = (user: Claims): IUser => ({
+    id: user.sub,
+    email: user.email,
+    name: user[`${namespace}/name`],
+    phone: user[`${namespace}/phone`],
+    cardcode: user[`${namespace}/cardcode`],
+});
 
-export { transformUser };
+export default { transformUser };
