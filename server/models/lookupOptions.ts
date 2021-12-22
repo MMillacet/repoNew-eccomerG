41bfded9f09d97664ce74b6bc63ad7adf402bf67
Nico <nisaul@protonmail.com>
2021-12-22 19:40:57 -0300

    Remove server

--- server/models/lookupOptions.ts
+++
@@ -1,4 +0,0 @@
-export interface LookupOptions {
-    itemcodes: string[];
-    cardcode: string;
-}
--- server/models/searchOptions.ts
+++
@@ -1,7 +0,0 @@
-export interface SearchOptions {
-    family: string;
-    category: string;
-    subcategory: string;
-    term: string;
-    orderBy: string;
-}
