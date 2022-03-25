b6b69ebc706b8bf003d26c5bf9a077338599db1e
Nico <nisaul@protonmail.com>
2022-03-25 15:53:13 -0300

    Fix header in mobile

--- src/components/mobile/MobileHeader.tsx
+++ src/components/mobile/MobileHeader.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState, memo, useCallback } from 'react';
 import classNames from 'classnames';
 
 // application
+import { useUser } from '@auth0/nextjs-auth0';
 import AppLink from '../shared/AppLink';
 import Cart20Svg from '../../svg/cart-20.svg';
 import Heart20Svg from '../../svg/heart-20.svg';
@@ -16,8 +17,12 @@ import url from '../../services/url';
 import { useCart } from '../../store/cart/cartHooks';
 import { useMobileMenuOpen } from '../../store/mobile-menu/mobileMenuHooks';
 import { useWishlist } from '../../store/wishlist/wishlistHooks';
+import IndicatorAccount from '../header/IndicatorAccount';
 
 function MobileHeader() {
+    const { user } = useUser();
+    const isUserActivated = user && !!user.cardcode;
+
     const [searchOpen, setSearchOpen] = useState(false);
     const inputRef = useRef<HTMLInputElement | null>(null);
     const cart = useCart();
@@ -49,22 +54,13 @@ function MobileHeader() {
             <div className="mobile-header__panel">
                 <div className="container">
                     <div className="mobile-header__body">
-                        <button
-                            type="button"
-                            className="mobile-header__menu-button"
-                            onClick={mobileMenuOpen}
-                        >
+                        <button type="button" className="mobile-header__menu-button" onClick={mobileMenuOpen}>
                             <Menu18x14Svg />
                         </button>
                         <AppLink href={url.home()} className="mobile-header__logo">
                             <img src="/images/logos/goldfarb-logo.png" alt="" />
                         </AppLink>
-                        <Search
-                            context="mobile-header"
-                            className={searchClasses}
-                            inputRef={inputRef}
-                            onClose={handleCloseSearch}
-                        />
+                        <Search context="mobile-header" className={searchClasses} inputRef={inputRef} onClose={handleCloseSearch} />
                         <div className="mobile-header__indicators">
                             <Indicator
                                 className="indicator--mobile indicator--mobile-search d-md-none"
@@ -77,12 +73,10 @@ function MobileHeader() {
                                 value={wishlistCount}
                                 icon={<Heart20Svg />}
                             />
-                            <Indicator
-                                className="indicator--mobile"
-                                url={url.cart()}
-                                value={cart.quantity}
-                                icon={<Cart20Svg />}
-                            />
+                            {isUserActivated && (
+                                <Indicator className="indicator--mobile" url={url.cart()} value={cart.quantity} icon={<Cart20Svg />} />
+                            )}
+                            <IndicatorAccount />
                         </div>
                     </div>
                 </div>
