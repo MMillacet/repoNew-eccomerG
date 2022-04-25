d2ef0c998480df956a0cf87811d0438246da42cf
Nico <nisaul@protonmail.com>
2022-04-25 18:13:55 -0300

    Fix auth product

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -1,18 +1,13 @@
-import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { getSession } from '@auth0/nextjs-auth0';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import goldfarbApi from '../../../api/goldfarb';
 
-export default withApiAuthRequired(async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
     const session = getSession(req, res);
 
     const { id } = req.query;
 
-    const cardcode = session && session.user.cardcode;
-
-    if (!cardcode) {
-        res.status(404).json({ error: 'No cardcode found' });
-        return;
-    }
+    const cardcode = session && session.user?.cardcode;
 
     try {
         const {
@@ -23,4 +18,4 @@ export default withApiAuthRequired(async (req: NextApiRequest, res: NextApiRespo
     } catch (error) {
         res.status(500).json({ error });
     }
-});
+};
