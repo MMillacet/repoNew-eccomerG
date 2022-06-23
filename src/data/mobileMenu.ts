44c5bd2310085cc96592ee74fa0a2741272774ad
Nico <nisaul@protonmail.com>
2022-06-22 21:34:05 -0300

    fix mobile menu

--- src/data/mobileMenu.ts
+++ src/data/mobileMenu.ts
@@ -2,11 +2,11 @@ import { IMobileMenu } from '../interfaces/menus/mobile-menu';
 
 const dataMobileMenu: IMobileMenu = [
     { type: 'link', title: 'HERRAMIENTAS', url: '/shop/catalog?family=Herramientas' },
-    { type: 'link', title: 'FERRETERÍA', url: '/shop/catalog?family=Ferreteria' },
-    { type: 'link', title: 'AGRO Y JARDIN', url: '/shop/catalog?family=Agro y Jardin' },
+    { type: 'link', title: 'FERRETERÍA', url: '/shop/catalog?family=Ferretería' },
+    { type: 'link', title: 'AGRO Y JARDIN', url: '/shop/catalog?family=Agro y Jardín' },
     { type: 'link', title: 'PINTURA', url: '/shop/catalog?family=Pinturas' },
     { type: 'link', title: 'BAZAR', url: '/shop/catalog?family=Bazar' },
-    { type: 'link', title: 'ELECTRODOMÉSTICOS', url: '/shop/catalog?family=Electrodomesticos' },
+    { type: 'link', title: 'ELECTRODOMÉSTICOS', url: '/shop/catalog?family=Electrodomésticos' },
     {
         type: 'link',
         title: 'SERVICIO TÉCNICO',
