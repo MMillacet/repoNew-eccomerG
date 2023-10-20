9e79d4ca4e045a537c6ebe753e426c3307480ee2
ObdulioR <obdulio.r@gmail.com>
2023-10-20 17:53:05 -0300

    Revert "Fix LiveAgent"
    This reverts commit 4291a26f5b38f4ea1c489c8f420aa9d7520dcfc3.

--- src/components/shared/LiveAgent.tsx
+++ src/components/shared/LiveAgent.tsx
@@ -4,8 +4,8 @@ const LiveAgentGF = () => {
   useEffect(() => {
     // Tu script JavaScript aquí
     (function(d, src, c) {
-      var t = d.scripts[d.scripts.length - 1] as any,
-        s = d.createElement('script') as any;
+      var t = d.scripts[d.scripts.length - 1],
+        s = d.createElement('script');
       s.id = 'la_x2s6df8d';
       s.defer = true;
       s.src = src;
@@ -17,7 +17,7 @@ const LiveAgentGF = () => {
         c(this);
       };
       t.parentElement.insertBefore(s, t.nextSibling);
-    })(document, 'https://goldfarb.ladesk.com/scripts/track.js', function(e: any) {            
+    })(document, 'https://goldfarb.ladesk.com/scripts/track.js', function(e) {
       LiveAgent.createButton('467lagv1', e);
     });
   }, []);
